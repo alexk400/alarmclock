7c4f0ab8c72fcb157798bfbb90f01f0322c728e6
Alex <alexkumar@gmail.com>
2024-09-25 19:21:36 -0700

    Issue #133: Test message1

--- c.bat
+++ c.bat
@@ -1,5 +1,25 @@
+REM @echo off
+REM git add -A
+REM git commit -m "Your default message"
+REM git push
+
+
+
 @echo off
-cd path_to_your_repo
+REM Change to your repository directory
+
+REM Check if a commit message was provided as a command line argument
+IF "%~1"== "" (
+    set commit_message="Your default message"
+) ELSE (
+    set commit_message=%~1
+)
+
+REM Add all changes to staging
 git add -A
-git commit -m "Your default message"
+
+REM Commit with the decided message
+git commit -m "%commit_message%"
+
+REM Push the commits
 git push
